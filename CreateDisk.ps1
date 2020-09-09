2ae66ec3f85f88fb6fea13707ae0d667e447c50e
Apaar Sharma <36641301+apaarshrm39@users.noreply.github.com>
2020-09-09 12:21:57 +0530

    Add files via upload

---
+++ CreateDisk.ps1
@@ -0,0 +1,5 @@
+﻿Get-Disk | 
+    Where partitionstyle -eq 'raw' |
+    Initialize-Disk -PartitionStyle MBR -PassThru |
+    New-Partition -DriveLetter "F" -UseMaximumSize |
+Format-Volume -FileSystem NTFS -NewFileSystemLabel "DataDisk" -Confirm:$false
